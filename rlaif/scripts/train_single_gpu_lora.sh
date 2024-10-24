a099284e4e8513eb41adaf3fc4fc3379f8b20e1e
Sampreeth-R-S <sampreeth2003@gmail.com>
2024-10-24 11:32:42 +0530

    added code for rst, esting pending

--- rlaif/scripts/train_single_gpu_lora.sh
+++ rlaif/scripts/train_single_gpu_lora.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
+#SBATCH --job-name=my_job
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+#SBATCH --time=15:00:00
+#SBATCH --partition=gpupart_p100
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=24gb
+source activate /home/du1/21CS30038/.conda/envs/pytorch_env
+pip install huggingface-hub
+huggingface-cli login --token "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"
+export WANDB_API_KEY="51bee7d3cdc3e9154459f935c5e4ea81ad2ef813"
 
-# replace these as necessary
-CONDA_PATH=/scratch/gpfs/$USER/envs/align
-module purge 
-module load anaconda3/2022.10
-conda activate $CONDA_PATH
-export TRANSFORMERS_OFFLINE=1
-export TRANSFORMERS_CACHE=/scratch/gpfs/ashwinee/rlaif-cache/
-export HF_HOME=/scratch/gpfs/ashwinee/rlaif-cache/
-export PROJECT_CACHE=/scratch/gpfs/ashwinee/rlaif-cache/
-export HF_DATASETS_OFFLINE=1
-export WANDB_MODE=offline
-export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
-export TORCH_DISTRIBUTED_DEBUG=OFF
 
 archive="${1:-mistralai/Mistral-7B-v0.1}"
 lr="${2:-5e-7}"
-dataset_name="${3:-sharegpt4}"
+dataset_name="${3:-gsm8k}"
 data_fraction="${4:-1.0}"
 n_epochs="${5:-3}"
 mask_dir="${6:-none}"
@@ -30,18 +29,15 @@ freeze_odd_layers="${13:-false}"
 freeze_even_layers="${14:-false}"
 
 model_archive=${archive}
-if [[ $archive == "/scratch/gpfs/ashwinee/rlaif-cache/sharegpt4_"* ]]; then
-    archive=${archive#"/scratch/gpfs/ashwinee/rlaif-cache/sharegpt4_"}
-fi
+
 # Replace commas with underscores to avoid issues with file paths or names
 sanitized_dataset_name=$(echo $dataset_name | tr ',' '_')
 exp_name="${sanitized_dataset_name}_${archive}_${grad_norm}_${lr}_${batch_size}_${data_fraction}_${mask_dir}_${sparsity_ratio}_LORA_${lora_rank}_${lora_alpha}"
 # exp_name="${sanitized_dataset_name}_${archive}_${grad_norm}_${lr}_${batch_size}_${data_fraction}_${mask_dir}_${sparsity_ratio}_DORA"
-model_save_path="/scratch/gpfs/ashwinee/rlaif-cache/${exp_name}/"
-trainer_type="FSDPTrainer"
+trainer_type="BasicTrainer"
 
-python -u train_single_gpu_lora.py do_first_eval=False \
-        mask_path=/scratch/gpfs/ashwinee/alignment-durability/masks/$mask_dir/${sparsity_ratio}_mask.pt \
+python -u ../train_single_gpu_lora.py do_first_eval=False \
+        mask_path=./masks/$mask_dir/${sparsity_ratio}_mask.pt \
         loss=sft \
         model=${model} \
         model.archive=${model_archive} \
@@ -58,8 +54,6 @@ python -u train_single_gpu_lora.py do_first_eval=False \
         n_epochs=$n_epochs \
         batch_size=$batch_size \
         gradient_accumulation_steps=1 \
-        model.fsdp_policy_mp=bfloat16 \
-        fsdp_port=${MASTER_PORT} \
         optimizer=RMSprop \
         grad_norm_strategy=even \
         max_grad_norm=$grad_norm \
@@ -68,10 +62,10 @@ python -u train_single_gpu_lora.py do_first_eval=False \
         freeze_odd_layers=$freeze_odd_layers \
         freeze_even_layers=$freeze_even_layers
 
-python convert_policy_to_hf_lora.py --lora_rank ${lora_rank} --model_path ${model_archive} --policy_path ${model_save_path}/epoch-$n_epochs/policy.pt --save_path ${model_save_path}/epoch-$n_epochs/
+# python ../convert_policy_to_hf_lora.py --lora_rank ${lora_rank} --model_path ${model_archive} --policy_path ${model_save_path}/epoch-$n_epochs/policy.pt --save_path ${model_save_path}/epoch-$n_epochs/
 
-python eval_model_all.py --model "${model_save_path}/epoch-$n_epochs/" --datasets "${dataset_name}"
+# python ../eval_model_all.py --model "${model_save_path}/epoch-$n_epochs/" --datasets "${dataset_name}"
 
-python generate_samples.py --prompt_set alpaca_eval --temperatures 0.7 --model_name ${exp_name} --model_path ${model_save_path}/epoch-$n_epochs/
+# python ../generate_samples.py --prompt_set alpaca_eval --temperatures 0.7 --model_name ${exp_name} --model_path ${model_save_path}/epoch-$n_epochs/
 
-python cleanup.py --model ${model_save_path}/epoch-$n_epochs/
+# python ../cleanup.py --model ${model_save_path}/epoch-$n_epochs/
